5108f96ccfc60bdb41fa94e19903952d9ff88d80
Zola Ojacastro <zola.ojacastro@my.jcu.edu.au>
2016-03-22 18:57:24 +1000

    Updated get_limits function to include error checking

--- basic_functions.py
+++ basic_functions.py
@@ -7,7 +7,7 @@ CP1404/CP5632 Workshop 04
 Basic functions
 demonstrating various parameters, returns and the use of a main function
 """
-__author__ = 'Lindsay Ward'
+__author__ = 'Zola Ojacastro'
 
 
 def main():
@@ -18,7 +18,10 @@ def main():
 
 def get_limits():
     minimum = int(input("Enter the minimum: "))
-    maximum = int(input("Enter the maximum: "))
+    maximum = int(input("Enter the maximum ({} or above): ".format(minimum)))
+    while minimum > maximum:
+        print("Maximum too low!")
+        maximum = int(input("Enter the maximum: "))
     return minimum, maximum
 
 
