13fd14a7651be4ebc1e048c149f826dc2bcd2101
Zola Ojacastro <zola.ojacastro@my.jcu.edu.au>
2016-03-29 15:13:28 +1000

    changed some minor things

---
+++ Workshop3/WordGenerator.py
@@ -0,0 +1,59 @@
+"""
+randomly generate words by constructing a string from random combinations of characters
+the word_format stores a sequence like ccvc, where c is a consonant and v is a vowel
+use random.choice function to select a single value from the sequence
+"""
+
+# import random
+#
+# VOWELS = "aeiou"
+# CONSONANTS = "bcdfghjklmnpqrstvyz"
+#
+# word_format = "ccvc"
+# word = ""
+# for kind in word_format:
+#     if kind == "c":
+#         word += random.choice(CONSONANTS)
+#     else:
+#         word += random.choice(VOWELS)
+#
+# print(word)
+
+# # ask the user for word format
+# import random
+#
+# VOWELS = "aeiou"
+# CONSONANTS = "bcdfghjklmnpqrstvyz"
+#
+# word_format = input("Create your own word format:")
+# word = ""
+# for kind in word_format:
+#     if kind == "#":
+#         word += random.choice(VOWELS)
+#     elif kind == "*" or kind == "%":
+#         word += random.choice(CONSONANTS)
+#     else:
+#         word += kind
+#
+# print(word)
+
+
+# #randomly generate word_format variable
+# import random
+#
+# VOWELS = "aeiou"
+# CONSONANTS = "bcdfghjklmnpqrstvyz"
+#
+# word_format = 
+# word = ""
+# for kind in word_format:
+#     if kind == "#":
+#         word += random.choice(VOWELS)
+#     elif kind == "*" or kind == "%":
+#         word += random.choice(CONSONANTS)
+#     else:
+#         word += kind
+#
+# print(word)
+#
+#
